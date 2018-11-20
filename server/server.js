95a6e47a26663b6a66cc148f3e7c9e4e17dbe1f0
Highyard <benj1619@stud.kea.dk>
2018-11-20 13:52:33 +0100

    Add tests for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -57,7 +57,7 @@ app.delete("/todos/:id", (req, res) => {
     if(!todo) {
       res.status(404).send();
     }
-    res.status(200).send(todo);
+    res.status(200).send({todo});
   }, (e) => {
     res.status(400).send();
   });
